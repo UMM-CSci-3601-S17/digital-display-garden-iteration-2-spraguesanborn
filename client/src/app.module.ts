0123263ccadc7b51379902af450f176becfb80f9
holma198 <holma198@morris.umn.edu>
2017-03-03 15:40:06 -0600

    Removed the kittens file and associated files

--- client/src/app.module.ts
+++ client/src/app.module.ts
@@ -5,7 +5,6 @@ import { HttpModule, JsonpModule } from '@angular/http';
 import { AppComponent }         from './app/app.component';
 import { NavbarComponent } from './app/navbar/navbar.component';
 import { HomeComponent} from './app/home/home.component';
-import { KittensComponent }   from './app/kittens/kittens.component';
 import { UserListComponent } from './app/users/user-list.component';
 import { UserListService } from './app/users/user-list.service';
 import { routing } from './app/app.routes';
@@ -24,7 +23,6 @@ import { PipeModule } from './pipe.module';
     ],
     declarations: [
         AppComponent,
-        KittensComponent,
         HomeComponent,
         NavbarComponent,
         UserListComponent
