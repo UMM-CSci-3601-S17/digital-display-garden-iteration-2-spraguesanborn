e3970da4093aa122b66e731d73f199659c9db38b
Tony Song <songx823@morris.umn.edu>
2017-03-03 16:22:56 -0600

    Basic server setup for flower api
    Issue: #10

---
+++ server/src/main/java/umm3601/flower/Flower.java
@@ -0,0 +1,10 @@
+package umm3601.flower;
+
+public class Flower {
+    String _id;
+    String commonName;
+    String cultivar;
+    String source;
+    String gardenLocation;
+    int year;
+}
---
+++ server/src/main/java/umm3601/flower/FlowerController.java
@@ -0,0 +1,80 @@
+package umm3601.flower;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.AggregateIterable;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class FlowerController {
+
+    private final MongoCollection<Document> flowerCollection;
+
+    public FlowerController() throws IOException {
+        // Set up our server address
+        // (Default host: 'localhost', default port: 27017)
+        // ServerAddress testAddress = new ServerAddress();
+
+        // Try connecting to the server
+        //MongoClient mongoClient = new MongoClient(testAddress, credentials);
+        MongoClient mongoClient = new MongoClient(); // Defaults!
+
+        // Try connecting to a database
+        MongoDatabase db = mongoClient.getDatabase("test");
+
+        flowerCollection = db.getCollection("flowers");
+    }
+
+    // List flowers
+    public String listFlowers(Map<String, String[]> queryParams) {
+        Document filterDoc = new Document();
+
+        if (queryParams.containsKey("year")) {
+            int targetAge = Integer.parseInt(queryParams.get("year")[0]);
+            filterDoc = filterDoc.append("year", targetAge);
+        }
+
+        FindIterable<Document> matchingFlowers = flowerCollection.find(filterDoc);
+
+        return JSON.serialize(matchingFlowers);
+    }
+
+    // Get a single flower
+    public String getFlower(String id) {
+        FindIterable<Document> jsonFlowers
+                = flowerCollection
+                .find(eq("_id", new ObjectId(id)));
+
+        Iterator<Document> iterator = jsonFlowers.iterator();
+
+        Document flower = iterator.next();
+
+        return flower.toJson();
+    }
+
+    // Get the average age of all users by company
+//    public String getAverageAgeByCompany() {
+//        AggregateIterable<Document> documents
+//                = userCollection.aggregate(
+//                Arrays.asList(
+//                        Aggregates.group("$company",
+//                                Accumulators.avg("averageAge", "$age")),
+//                        Aggregates.sort(Sorts.ascending("_id"))
+//                ));
+//        System.err.println(JSON.serialize(documents));
+//        return JSON.serialize(documents);
+//    }
+}
