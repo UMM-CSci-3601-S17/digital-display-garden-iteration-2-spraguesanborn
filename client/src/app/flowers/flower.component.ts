91a281d45f0521fb660395d35cbbfe7100800f8c
holma198 <holma198@morris.umn.edu>
2017-03-07 12:59:02 -0600

    Fixed The Comment Box!  No more Navbar Issues!

--- client/src/app/flowers/flower.component.ts
+++ client/src/app/flowers/flower.component.ts
@@ -1,20 +1,39 @@
-import { Component, Input, Output, EventEmitter, ElementRef} from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Feedback } from './feedback';
+
+
 
 @Component({
     templateUrl: 'flower.component.html',
     styles: ['button.responseButton { border: 2px solid #4CAF50; width: 49%; height: 60px}',
         'span.glyphicon {font-size: 40px;}', 'input.commentBox {width: 99%; font-size: 200%; height: 80px;border: 2px solid #4CAF50;}',
         'button.submitButton {width: 60%; height:60px; border: 2px solid #4CAF50; font-size: 40px;' +
-        ' display: block; margin: auto;}', 'div.fullComment {width: 99%; margin: auto;}', 'hr.flowerPageHR {border: 1px solid #4CAF50;}']
+        ' display: block; margin: auto;}', 'div.fullComment {width: 99%; margin: auto;}', 'hr.flowerPageHR {border: 1px solid #4CAF50;}'],
+    selector: 'my-app',
 })
 
 // Component class
 export class FlowerComponent {
     public text: string;
+    public myForm: FormGroup; // our model driven form
+    public submitted: boolean; // keep track on whether form is submitted
+    public events: any[] = []; // use later to display form changes
 
+    constructor(private _fb: FormBuilder) { } // form builder simplify form initialization
 
-    constructor() {
-        this.text = "Information about flower";
+    ngOnInit() {
+        this.myForm = this._fb.group({
+            comment: ['', [<any>Validators.required]],
+        });
+    }
+
+    save(model: Feedback, isValid: boolean) {
+        this.submitted = true; // set form submit to true
+
+        // check if model is valid
+        // if valid, call API to save customer
+        console.log(model, isValid);
     }
 
 }
