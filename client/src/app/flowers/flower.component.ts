d5aeae42a9720ea6df7a02b02299e0f68eddc8a2
holma198 <holma198@morris.umn.edu>
2017-03-06 20:57:16 -0600

    Comment out the new comment box, to fix html issue with the navbar

--- client/src/app/flowers/flower.component.ts
+++ client/src/app/flowers/flower.component.ts
@@ -4,14 +4,14 @@ import { Component, Input, Output, EventEmitter, ElementRef} from '@angular/core
     templateUrl: 'flower.component.html',
     styles: ['button.responseButton { border: 2px solid #4CAF50; width: 49%; height: 60px}',
         'span.glyphicon {font-size: 40px;}', 'input.commentBox {width: 99%; font-size: 200%; height: 80px;border: 2px solid #4CAF50;}',
-        'input.submitButton {width: 60%; height:60px; border: 2px solid #4CAF50; font-size: 40px;' +
+        'button.submitButton {width: 60%; height:60px; border: 2px solid #4CAF50; font-size: 40px;' +
         ' display: block; margin: auto;}', 'div.fullComment {width: 99%; margin: auto;}', 'hr.flowerPageHR {border: 1px solid #4CAF50;}']
 })
 
 // Component class
 export class FlowerComponent {
     public text: string;
-    public submit: boolean;
+
 
     constructor() {
         this.text = "Information about flower";
