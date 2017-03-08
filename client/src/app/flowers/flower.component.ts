368c7f41d29e0f808b81806304eb289f2f12e3c3
Ai Sano <sanox013@morris.umn.edu>
2017-03-08 13:51:47 -0600

    Comment out the original styles

--- client/src/app/flowers/flower.component.ts
+++ client/src/app/flowers/flower.component.ts
@@ -8,12 +8,11 @@ import { Feedback } from './feedback';
 
 @Component({
     templateUrl: 'flower.component.html',
-    styles: ['button.responseButton { border: 2px solid #4CAF50; width: 49%; height: 60px}',
+    /*styles: ['button.responseButton { border: 2px solid #4CAF50; width: 49%; height: 60px}',
         'span.glyphicon {font-size: 40px;}', 'input.commentBox {width: 99%; font-size: 200%; height: 80px;border: 2px solid #4CAF50;}',
         'button.submitButton {width: 60%; height:60px; border: 2px solid #4CAF50; font-size: 40px;' +
         ' display: block; margin: auto;}', 'div.fullComment {width: 99%; margin: auto;}', 'hr.flowerPageHR {border: 1px solid #4CAF50;}',
-        'li.active {font-size: 30px; padding-bottom: 15px; border-bottom: solid green;}'],
-
+        'li.active {font-size: 30px; padding-bottom: 15px; border-bottom: solid green;}'],*/
     selector: 'my-app',
 })
 
