9d6cceb8078c7606d5ef50b3daeae5b190aa9d68
hamme503 <hamme503@morris.umn.edu>
2017-03-25 15:37:13 -0500

    Re-added import statements for admin component on relevant files
    Issue #1 #5

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {UserListComponent} from "./users/user-list.component";
-import {FlowerComponent} from "./flowers/flower.component"
+import {FlowerComponent} from "./flowers/flower.component";
+import { AdminComponent } from "./admin/admin.component"
 
 // Route Configuration
 export const routes: Routes = [
     { path: '', component: FlowerComponent },
     { path: 'users', component: UserListComponent },
     { path: 'flowers', component: FlowerComponent },
+    { path: 'admin', component: AdminComponent },
     { path: '**', component: FlowerComponent }
 ];
 
